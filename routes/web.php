f45d77e9fcaba27bd08a8129c38df7451cf929cc
Momcilo-Petkovic <momcilopetkovic27@gmail.com>
2022-08-15 18:08:58 +0200

    Implemented Multi Auth (You can seperatly log in as an Admin or as a User)

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,10 +18,19 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified'
+Route::middleware('admin:admin')->group(function () {
+    Route::get('admin/login', [AdminController::class, 'loginForm']);
+    Route::post('admin/login', [AdminController::class, 'store'])->name('admin.login');
+});
+
+Route::middleware(['auth:sanctum,admin', config('jetstream.auth_session'), 'verified'
+])->group(function () {
+    Route::get('/admin/dashboard', function () {
+        return view('dashboard'); // Ovde bih menjao kom dashboardu zelim admin da pristupi
+    })->name('dashboard')->middleware('auth:admin');
+});
+
+Route::middleware(['auth:sanctum', config('jetstream.auth_session'), 'verified'
 ])->group(function () {
     Route::get('/dashboard', function () {
         return view('dashboard');
